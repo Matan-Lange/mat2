b680ce85deca1ecac9f5e794affff926300ea784
Matan Lange <matanlange88@gmail.com>
2021-03-25 11:02:08 +0200

    first

---
+++ text.py
@@ -0,0 +1,29 @@
+def revowrd(word: str) -> str:
+    return word.lower()[::-1]
+
+
+def countword() -> int:
+    file = open('text.txt', 'r')
+    words = file.read().split()
+
+    word = words[0]
+    file.seek(0)
+    text_list = file.readlines()[1:]
+    file.close()
+
+    file = open('text.txt', 'w')
+    file.write(word + '\n')
+
+    counter = 1
+
+    for line in text_list:
+        newline = line.rstrip('\n')
+
+        for w in newline.split():
+            if revowrd(w) == word:
+                counter += 1
+            file.write(revowrd(w) + " ")
+        file.write('\n')
+
+    file.close()
+    return counter
